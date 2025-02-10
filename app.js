0cd3df12773510fa7258993b0bc3fde2e2dc1901
delapenhadev <=>
2025-02-09 22:55:11 -0300

    Criando array de nomes no jogo

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+let lista = document.querySelector("input");
+let result = document.querySelector("#resultado");
+
+function adicionarAmigo(){
+  let nome = lista.value;
+  if (nome){
+    amigos.push(nome);
+    result.innerHTML = amigos.join("<br>");
+    lista.value = "";
+  }else{
+    alert("Por favor insirar um valor");
+  }
+  
+}
+
+
+
+
+
+
+
+
